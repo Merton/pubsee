13526723baeeb23518ae47b61418cdf00aa034e9
Merton <mertlansley@gmail.com>
2017-09-20 17:26:29 +0100

    Changed api view to reflect local authority

--- api/views.py
+++ api/views.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.generics import ListCreateAPIView
 from .serializers import PubsSerializer
-from pubs.models import Pub
+from pubs.models import Pub, Local_Authority
 # Create your views here.
 class PubsView(ListCreateAPIView):
     serializer_class = PubsSerializer
